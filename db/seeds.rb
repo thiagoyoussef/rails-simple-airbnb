e696480dfb13b6ade524b4b7f883a5646b40b595
Thiago Youssef <thiagodyoussef@gmail.com>
2021-05-26 16:51:53 -0300

    Challenge Solved

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+10.times do |_|
+  Flat.create!(
+    name: Faker::Name.name,
+    address: Faker::Address.full_address,
+    description: Faker::TvShows::GameOfThrones.quote,
+    price_per_night: (50..600).to_a.sample,
+    number_of_guests: (1..10).to_a.sample
+  )
+end
+
+puts "Finished!"
